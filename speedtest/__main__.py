de89de2d6d9e24a17fea164c788329c430df2b90
Michał Korczak <git@omikor.in>
2026-06-10 22:52:47 +0000

    refactor: main

--- speedtest/__main__.py
+++ speedtest/__main__.py
@@ -1,6 +1,5 @@
 """
 The main entry point. Invoke as `speedtest-cli` or `python -m speedtest`.
-
 """
 
 import sys
@@ -9,30 +8,33 @@ from speedtest.exceptions import SpeedtestException
 from speedtest.status import ExitStatus
 
 
-def main():
+def main() -> int:
     try:
         from speedtest.cli import shell
 
         exit_status = shell()
+
     except KeyboardInterrupt:
-        print("Stopped by user")
-
+        print("Stopped by user", file=sys.stderr)
         exit_status = ExitStatus.ERROR_CTRL_C
+
     except SpeedtestException as e:
-        # TODO: Rework
+        code = getattr(e, "code", 1)
 
-        if getattr(e, "code", 1) not in (ExitStatus.SUCCESS, ExitStatus.ERROR_CTRL_C):
-            msg = "%s" % e
-            if not msg:
-                msg = "%r" % e
-            raise SystemExit("ERROR: %s" % msg)
+        if code not in (ExitStatus.SUCCESS, ExitStatus.ERROR_CTRL_C):
+            msg = str(e) or repr(e)
+            raise SystemExit(f"ERROR: {msg}") from e
 
-    return exit_status.value
+        exit_status = code
+
+    return getattr(exit_status, "value", exit_status)
 
 
 if __name__ == "__main__":
-    # TODO: Check if this is needed
-    sys.stdout.reconfigure(encoding="utf-8")
-    sys.stderr.reconfigure(encoding="utf-8")
+    try:
+        sys.stdout.reconfigure(encoding="utf-8")
+        sys.stderr.reconfigure(encoding="utf-8")
+    except AttributeError:
+        pass
 
     sys.exit(main())
