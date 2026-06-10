52601047c3dce8bc6a093e3f872f6293e4771be7
Michał Korczak <git@omikor.in>
2026-06-10 17:17:44 +0000

    refactor: setup

---
+++ speedtest/__init__.py
@@ -0,0 +1,16 @@
+"""
+speedtest-cli-ng: Command line interface for testing internet bandwidth using speedtest.net
+
+This project is a continuation/fork of the original "speedtest-cli" by Matt Martz.
+
+Licensed under the Apache License, Version 2.0.
+
+Copyright (c) 2012-2026 Matt Martz
+Copyright (c) 2026 Michał Korczak
+
+"""
+
+__version__ = "0.1.0"
+__date__ = "2026-06-10"
+__author__ = "Michał Korczak"
+__licence__ = "Apache License, Version 2.0"
