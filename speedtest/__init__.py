8c113e2b4e59458769fe5c42025f3ae4a5c37456
Michał Korczak <git@omikor.in>
2026-06-16 00:36:40 +0000

    refactor: there is a notice for that

--- speedtest/__init__.py
+++ speedtest/__init__.py
@@ -1,13 +1,6 @@
 """
 speedtest-cli: Command line interface for testing internet bandwidth using speedtest.net
 
-This project is a continuation/fork of the original "speedtest-cli" by Matt Martz.
-
-Licensed under the Apache License, Version 2.0.
-
-Copyright (c) 2026 Michał Korczak
-Copyright (c) 2012-2026 Matt Martz
-
 """
 
 from importlib.metadata import PackageNotFoundError, version
