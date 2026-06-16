879a71008de5fcd587e24e005bec99f4fa4178a8
Michał Korczak <6079265+Omikorin@users.noreply.github.com>
2026-06-16 00:33:08 +0000

    build: make possible using uv version

--- speedtest/__init__.py
+++ speedtest/__init__.py
@@ -10,7 +10,13 @@ Copyright (c) 2012-2026 Matt Martz
 
 """
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("speedtest-cli")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 __date__ = "2026-06-10"
 __author__ = "Michał Korczak"
 __licence__ = "Apache License, Version 2.0"
