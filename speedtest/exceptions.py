487f5be1ab8764b358d5ef2354ea8dfc1cb2dd0f
Michał Korczak <git@omikor.in>
2026-06-21 19:16:26 +0200

    chore: clean

--- speedtest/exceptions.py
+++ speedtest/exceptions.py
@@ -3,18 +3,10 @@ Speedtest-related exceptions.
 """
 
 __all__ = [
-    "ConfigRetrievalError",
     "NoMatchedServer",
-    "ServersRetrievalError",
-    "ShareResultsConnectFailure",
-    "ShareResultsSubmitFailure",
     "SpeedtestBestServerFailure",
     "SpeedtestCLIError",
-    "SpeedtestConfigError",
     "SpeedtestException",
-    "SpeedtestHTTPError",
-    "SpeedtestMissingBestServer",
-    "SpeedtestServersError",
     "SpeedtestUploadTimeout",
 ]
 
@@ -27,39 +19,8 @@ class SpeedtestCLIError(SpeedtestException):
     """Generic exception for raising errors during CLI operation."""
 
 
-class SpeedtestHTTPError(SpeedtestException):
-    """Base HTTP exception for this module."""
-
-
-class SpeedtestConfigError(SpeedtestException):
-    """Configuration XML is invalid."""
-
-
-class SpeedtestServersError(SpeedtestException):
-    """Servers XML is invalid."""
-
-
-class ConfigRetrievalError(SpeedtestHTTPError):
-    """Could not retrieve config.php."""
-
-
-class ServersRetrievalError(SpeedtestHTTPError):
-    """Could not retrieve speedtest-servers.php."""
-
-
 class NoMatchedServer(SpeedtestException):
     """No server matched when filtering."""
-
-
-class ShareResultsConnectFailure(SpeedtestException):
-    """Could not connect to speedtest.net API to POST results."""
-
-
-class ShareResultsSubmitFailure(SpeedtestException):
-    """
-    Unable to successfully POST results to speedtest.net API after
-    connection.
-    """
 
 
 class SpeedtestUploadTimeout(SpeedtestException):
@@ -72,7 +33,3 @@ class SpeedtestUploadTimeout(SpeedtestException):
 
 class SpeedtestBestServerFailure(SpeedtestException):
     """Unable to determine best server."""
-
-
-class SpeedtestMissingBestServer(SpeedtestException):
-    """get_best_server not called or not able to determine best server."""
